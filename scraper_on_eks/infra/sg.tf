2a7d304a73ee6a5b9cc16c5197b4f2ff5a8281ae
Max Brydak <maxbrydak@gmail.com>
2022-09-11 00:27:16 +0200

    iam test

--- scraper_on_eks/infra/sg.tf
+++ scraper_on_eks/infra/sg.tf
@@ -2,17 +2,17 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-# module "bastion_sg" {
-#   source  = "terraform-aws-modules/security-group/aws//modules/ssh"
-#   version = "~> 4.0"
+module "bastion_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "~> 4.0"
 
-#   name                = "bastion-sg-${random_id.id.hex}"
-#   description         = "allow ssh access to bastion host"
-#   vpc_id              = module.vpc_eks.vpc_id
-#   ingress_cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  name                = "bastion-sg-${random_id.id.hex}"
+  description         = "allow ssh access to bastion host"
+  vpc_id              = module.vpc_eks.vpc_id
+  ingress_cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
 
-#   tags = {
-#     "name" = "bastion-sg--${random_id.id.hex}"
-#   }
-# }
+  tags = {
+    "name" = "bastion-sg--${random_id.id.hex}"
+  }
+}
 
